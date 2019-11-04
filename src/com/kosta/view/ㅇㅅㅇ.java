f2139373e4d14bfc16fbfc39fc9a1ac38cc4fe19
kosta <kosta@192.168.0.100>
2019-11-04 18:15:14 +0900

    ver3

--- src/com/kosta/view/ㅇㅅㅇ.java
+++ src/com/kosta/view/ㅇㅅㅇ.java
@@ -4,6 +4,12 @@ public class しさし {
 	public static void main(String[] args) {
 		while(true) {
 		System.out.println("しさし");
+		System.out.println("しさし");
+		System.out.println("しさし");
+		System.out.println("しさし");
+		System.out.println("しさし");
+		System.out.println("しさし");
+		System.out.println("しさし");
 		}
 	}
 }
