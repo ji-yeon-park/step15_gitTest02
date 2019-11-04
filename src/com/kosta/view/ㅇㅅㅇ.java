ef344caf4223631df1b2c5cf93eeec2a06d17dda
kosta <kosta@192.168.0.103>
2019-11-04 18:24:33 +0900

    지연 수정

--- src/com/kosta/view/ㅇㅅㅇ.java
+++ src/com/kosta/view/ㅇㅅㅇ.java
@@ -4,6 +4,11 @@ public class ㅇㅅㅇ {
 	public static void main(String[] args) {
 		while(true) {
 		System.out.println("view 수정함 ");
+		System.out.println("view 수정함 ");
+		System.out.println("view 수정함 ");
+		System.out.println("view 수정함 ");
+		System.out.println("view 수정함 ");
+		
 		}
 	}
 }
